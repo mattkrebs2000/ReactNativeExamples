35cb469d1e151a3f3851c208750ff1638fdc388e
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-09-12 15:46:52 -0400

    new folders

--- App.js
+++ App.js
@@ -1,109 +1,23 @@
-import React, { Component } from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import Score from "./components/Score";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import Header from "./components/Header";
-import Waces from "./cards.json";
-import { MediaQuery } from "react-native-responsive-ui";
 
-
-class App extends Component {
-  state = {
-    Waces,
-    clickedFacesIds: [],
-    score: 0,
-    goal: 8,
-    imagenumber: 0,
-    status: " ",
-    direction: " ",
-  };
-
-  shuffleScoreCard = (id, name) => {
-    // debugger
-    this.setState({
-      imagenumber: this.state.imagenumber > 2 ? 0 : this.state.imagenumber + 1,
-      status: " " + name,
-    });
-    let clickedFacesIds = this.state.clickedFacesIds;
-    if (clickedFacesIds.includes(id)) {
-      this.setState({
-        clickedFacesIds: [],
-        score: 0,
-        status: "Game Over! You clicked " + name + " twice.",
-        direction: "Click to play again!",
-      });
-
-      return;
-    } else {
-      clickedFacesIds.push(id);
-      this.setState({ direction: name });
-
-      if (clickedFacesIds.length === 8) {
-        this.setState({
-          score: 8,
-          status: "You Won!",
-          clickedFacesIds: [],
-          direction: "Click to play again!",
-        });
-        console.log("You Win");
-        return;
-      }
-      
-      this.setState({
-        Waces,
-        clickedFacesIds,
-        score: clickedFacesIds.length,
-        status: " ",
-      });
-
-      for (let i = Waces.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-
-        //reorganizes json file
-        [Waces[i], Waces[j]] = [Waces[j], Waces[i]];
-
-      
-      }
-    }
-  };
-
-  render() {
-    console.log("WHTAT")
-    return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Header />
-          <Score
-            total={this.state.score}
-            goal={8}
-            status={this.state.status}
-            direction={this.state.direction}
-          />
-
-          <Wrapper>
-            
-            {this.state.Waces.map((face) => (
-              <Card
-                shuffleScoreCard={this.shuffleScoreCard}
-                id={face.id}
-                key={face.id}
-                image={face.image[this.state.imagenumber]}
-                name={face.name}
-              />
-            ))}
-          </Wrapper>
-        </View>
-      </ScrollView>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default App;
